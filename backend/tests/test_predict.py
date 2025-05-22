f4973932b7a26d0dba0c3dc0d4f60eb7cee74e7f
samiribrh <samiribrahimov2277@gmail.com>
2025-05-22 18:48:12 +0200

    Adding authentication to the test

--- backend/tests/test_predict.py
+++ backend/tests/test_predict.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 
+from backend.app.core.config import settings
 from backend.main import app
 
 
@@ -7,15 +8,19 @@ client = TestClient(app)
 
 
 def test_predict_valid_input():
-    response = client.post("/v1/predict/predict", json={
-        "pm25_lag_1": 8.5,
-        "pm25_lag_2": 8.2,
-        "pm25_lag_3": 9.0,
-        "year": 2024,
-        "month": 6,
-        "sin_month": 0.5,
-        "cos_month": 0.87
-    })
+    response = client.post(
+        "/v1/predict/predict",
+        headers={"x-api-key": settings.API_KEY},
+        json={
+            "pm25_lag_1": 8.5,
+            "pm25_lag_2": 8.2,
+            "pm25_lag_3": 9.0,
+            "year": 2024,
+            "month": 6,
+            "sin_month": 0.5,
+            "cos_month": 0.87
+        }
+    )
     assert response.status_code == 200
     json_data = response.json()
     assert "predicted_pm25" in json_data
@@ -23,13 +28,17 @@ def test_predict_valid_input():
 
 
 def test_predict_invalid_input():
-    response = client.post("/v1/predict/predict", json={
-        "pm25_lag_1": "bad",  # invalid type
-        "pm25_lag_2": 8.2,
-        "pm25_lag_3": 9.0,
-        "year": 2024,
-        "month": 6,
-        "sin_month": 0.5,
-        "cos_month": 0.87
-    })
+    response = client.post(
+        "/v1/predict/predict",
+        headers={"x-api-key": settings.API_KEY},
+        json={
+            "pm25_lag_1": "bad",  # invalid type
+            "pm25_lag_2": 8.2,
+            "pm25_lag_3": 9.0,
+            "year": 2024,
+            "month": 6,
+            "sin_month": 0.5,
+            "cos_month": 0.87
+        }
+    )
     assert response.status_code == 422
